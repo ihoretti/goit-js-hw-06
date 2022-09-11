9e94b7bbe15c850f15c44a8729fab4797954266e
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-09-12 00:14:12 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -17,9 +17,12 @@ const buttonChange = document.querySelector("button");
 buttonChange.addEventListener("click", colorChange);
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
+const textColor = document.querySelector(".color");
 function colorChange() {
   let color = getRandomHexColor();
   document.body.setAttribute("style", `background-color: ${color}`);
